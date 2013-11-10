46b10af4bdc075aa56f8db37b85f1cf1e9fdba4b
Daniel Duckworth <duckworthd@Daniels-MacBook-Pro.local>
2013-11-10 14:03:25 -0800

    setup: install commonly used mac apps

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env sh -xe
 
 # show command, then execute it
 function show() {
@@ -16,10 +16,84 @@ for FNAME in $(find $PROJECT_DIR \
                     -maxdepth 1 \
                     \( ! -iname ".git*" -or -iname '.dropbox' \)
               ); do
-  OUTPUT=~/$(basename "$FNAME")
+  OUTPUT=$HOME/$(basename "$FNAME")
   # if $OUTPUT isn't a file or a directory
   if [ ! -f "$OUTPUT" ] && [ ! -d "$OUTPUT" ]; then
     # softlink $FNAME to $OUTPUT
     show ln -s "$FNAME" "$OUTPUT"
   fi
 done
+
+# install Homebrew
+cd /tmp && ruby -e "$(curl -fsSL "https://raw.github.com/mxcl/homebrew/go")"
+brew update
+
+# install XCode
+brew install coreutils
+open "https://developer.apple.com/downloads/index.action"
+echo "Download and install XCode (press ENTER when done)"
+read TRASH
+
+# install tmux
+brew install tmux
+brew install reattach-to-user-namespace
+
+# install htop
+brew install htop
+
+# install zsh
+brew install zsh
+chsh -s $(which zsh)
+
+# install MacVim + ctags
+brew install macvim --with-lua --override-system-vim
+brew install ctags
+
+# install Alfred
+cd $HOME/Downloads
+curl "http://cachefly.alfredapp.com/Alfred_2.1_218.zip" > alfred.zip
+sudo unzip alfred.zip -d /Applications
+rm -f alfred.zip
+
+# Install Slate
+curl "http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz" | sudo tar -xz -C "/Applications"
+
+# Install Chrome
+curl "https://dl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg" > "$HOME/Downloads/chrome.dmg" && open "$HOME/Downloads/chrome.dmg"
+echo "Install Chrome (press ENTER to continue)"
+read TRASH
+
+# Install KeypassX
+curl "http://downloads.sourceforge.net/keepassx/KeePassX-0.4.3.dmg" > "$HOME/Downloads/keepassx.dmg" && "open keypassx.dmg"
+echo "Install KeypassX (press ENTER to continue)"
+read TRASH
+
+# Install Dropbox
+curl "https://www.dropbox.com/downloading?os=mac" > "$HOME/Downloads/dropbox.dmg" && open "$HOME/Downloads/dropbox.dmg"
+echo "Install Dropbox (press ENTER to continue)"
+read TRASH
+
+# Install JVM
+curl 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=81813' > '$HOME/Downloads/jre.dmg' && open '$HOME/Downloads/jre.dmg'
+echo "Install Java Runtime Environment (press ENTER to continue)"
+read TRASH
+
+# Install MySQL
+brew install mysql
+
+# install jq
+brew install jq
+
+# install scala
+brew install scala
+
+# Install R
+brew tap homebrew/science
+brew install R
+
+# Install Python + libraries
+brew install python gfortran freetype libevent
+pip install numpy scipy matplotlib pandas IPython ipdb virtualenv     # scientific
+pip install funcy duxlib configurati virtualenv lxml python-dateutil  # productivity
+pip install sqlrest gevent MySQL-python bottle pyjade                 # web
+pip install nose                                                      # testing
