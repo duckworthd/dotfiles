8d31271b8498fdc110969429c9370eaac6a90409
Daniel Duckworth <duckworthd@Daniels-MacBook-Pro.local>
2013-11-11 12:03:52 -0800

    mysql: boot on startup

--- installs/mysql.sh
+++ installs/mysql.sh
@@ -2,3 +2,6 @@
 # Install MySQL
 brew install mysql
 
+# start mysql on system boot
+ln -sfv /usr/local/opt/mysql/*.plist $HOME/Library/LaunchAgents
+launchctl load $HOME/Library/LaunchAgents/homebrew.mxcl.mysql.plist
