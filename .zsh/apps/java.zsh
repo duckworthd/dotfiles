f243aa1018da1aeb0021b98cd3ca38e197551b9c
Daniel Duckworth <dux@premise.com>
2014-04-23 21:27:36 -0700

    java: set VM size to 4g

--- .zsh/apps/java.zsh
+++ .zsh/apps/java.zsh
@@ -1,4 +1,4 @@
 # Suppress java cli processes from Dock (as of 1.6.0_51)
 # http://prod.lists.apple.com/archives/java-dev/2013/Jun/msg00080.html
 # http://stackoverflow.com/a/13849939/397334
-export _JAVA_OPTIONS="$_JAVA_OPTIONS -Djava.awt.headless=true"
+export _JAVA_OPTIONS="$_JAVA_OPTIONS -Djava.awt.headless=true -Xmx4g"
