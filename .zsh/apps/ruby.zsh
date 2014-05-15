7ce5114b7407ffe64e779552266229a421332c75
Daniel Duckworth <dux@premise.com>
2014-05-14 22:12:17 -0700

    tmuxinator configuration

--- .zsh/apps/ruby.zsh
+++ .zsh/apps/ruby.zsh
@@ -1,3 +1,9 @@
+# initalize rbenv
 if hash rbenv 2> /dev/null; then
   eval "$(rbenv init -)"
+fi
+
+# initialize tmuxinator autocompletion
+if [[ -e "~/.bin" ]] && [[ -e "~/.bin/tmuxinator.zsh" ]]; then
+  source ~/.bin/tmuxinator.zsh
 fi
