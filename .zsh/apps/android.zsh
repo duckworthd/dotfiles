f4bb66b0d08a6a7bccb0c615fb178d986efcf3a8
Daniel Duckworth <duckworthd@gmail.com>
2014-05-24 18:58:00 -0700

    zsh: android, eclipse specific functionality

---
+++ .zsh/apps/android.zsh
@@ -0,0 +1,4 @@
+# root for android, as managed by `brew cask install android-sdk`
+if [[ -e /usr/local/opt/android-sdk ]]; then
+  export ANDROID_HOME=/usr/local/opt/android-sdk
+fi
---
+++ .zsh/apps/eclipse.zsh
@@ -0,0 +1,11 @@
+
+# This will set "$?" to 1 if eclipse isn't installed, and 0 if it is
+brew cask list eclipse-ide &> /dev/null
+
+if [[ $? = 0 ]]; then
+  # get root folder of currently linked version of eclipse
+  ECLIPSE_ROOT=$(brew cask info eclipse-ide | sed '3q;d' | cut -d' ' -f1)
+
+  # add it to PATH (necessary for using eclimd)
+  export PATH="$ECLIPSE_ROOT/eclipse:$PATH"
+fi
