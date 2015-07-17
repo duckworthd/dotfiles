2ba841c484e86d4c76721c424c5d35b545cb64b2
Daniel Duckworth <duckworthd@google.com>
2015-07-17 11:36:54 -0700

    zsh: add tmux-update-environment to reload environment variables like DISPLAY (fixes xclip, xsel, etc)

--- dotfiles/zsh/apps/tmux.zsh
+++ dotfiles/zsh/apps/tmux.zsh
@@ -4,3 +4,21 @@ if command_exists reattach-to-user-namespace; then
     alias $cmd="reattach-to-user-namespace $cmd"
   done
 fi
+
+# Run this function from within tmux to update environment variables in the
+# tmux session. This is critical to keeping ssh forwarding and xclip working
+# when re-attaching to a tmux session.
+tmux-update-environment () {
+   echo -n "Updating to latest tmux environment...";
+    export IFS=",";
+    for line in $(tmux showenv -t $(tmux display -p "#S") | tr "\n" ",");
+    do
+        if [[ $line == -* ]]; then
+            unset $(echo $line | cut -c2-);
+        else
+            export $line;
+        fi;
+    done;
+    unset IFS;
+    echo "Done"
+}
