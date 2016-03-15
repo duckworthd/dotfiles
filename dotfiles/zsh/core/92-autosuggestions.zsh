cc571e4632feb1d10f38cd80dc2c40013ceff94e
Daniel Duckworth <duckworthd@google.com>
2016-03-15 11:03:54 -0700

    zsh: update zsh-autosuggestions

--- dotfiles/zsh/core/92-autosuggestions.zsh
+++ dotfiles/zsh/core/92-autosuggestions.zsh
@@ -1,11 +1,2 @@
-# Initialize syntax highlighting (prerequisite for autosuggestions).
-source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 # Initialize autosuggestions.
-source ~/.zsh/plugins/zsh-autosuggestions/autosuggestions.zsh
-
-# Register autosuggestions for auto-initialization.
-zle-line-init() {
-  zle autosuggest-start
-}
-zle -N zle-line-init
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
