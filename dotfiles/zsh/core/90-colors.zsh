003205e35ff16c0c15af15bbe5ad131d0dabb22f
Daniel Duckworth <duckworthd@google.com>
2016-04-04 14:37:06 -0700

    zsh: fixed unescaped color reset code

--- dotfiles/zsh/core/90-colors.zsh
+++ dotfiles/zsh/core/90-colors.zsh
@@ -1,4 +1,4 @@
-autoload colors; colors
+autoload -U colors; colors
 
 # The variables are wrapped in %{%}. This should be the case for every
 # variable that does not contain space.
@@ -7,7 +7,7 @@ for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
   eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
 done
 
-eval RESET='$reset_color'
+eval RESET='%{$reset_color%}'
 export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK \
   PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE PR_BOLD_WHITE \
   PR_BOLD_BLACK
