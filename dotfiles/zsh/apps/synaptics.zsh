830b34680643ceab7d0f9eec0bccfcfd9e9c7662
Daniel Duckworth <duckworthd@gmail.com>
2017-08-14 11:31:03 -0700

    zsh: hide synclient stderr output

--- dotfiles/zsh/apps/synaptics.zsh
+++ dotfiles/zsh/apps/synaptics.zsh
@@ -1,5 +1,6 @@
 if [[ "$(uname)" = "Linux" ]] && command_exists synclient; then
-  # Disable right click on touchpads
-  synclient RightButtonAreaLeft=0
-  synclient RightButtonAreaTop=0
+  # Disable right click on touchpads. If there is no synpatics driver loaded,
+  # this typically prints to stderr. We hide that here.
+  synclient RightButtonAreaLeft=0 2> /dev/null
+  synclient RightButtonAreaTop=0 2> /dev/null
 fi
