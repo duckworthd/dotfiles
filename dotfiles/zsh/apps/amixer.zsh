8a85c3dbac45ed607fd4871bb0b4adfce3322964
Daniel Duckworth <duckworthd@gmail.com>
2017-09-04 16:39:52 -0700

    zsh: amixer, --device instead of -D

--- dotfiles/zsh/apps/amixer.zsh
+++ dotfiles/zsh/apps/amixer.zsh
@@ -1,6 +1,6 @@
 PLATFORM=$(uname)
 if [[ ${PLATFORM} -eq "Linux" ]]; then
-  alias volume="amixer -D pulse sset Master"
+  alias volume="amixer --device pulse set Master"
 elif [[ ${PLATFORM} -eq "Darwin" ]]; then
   alias volume="osascript -e 'set Volume'"
 fi
