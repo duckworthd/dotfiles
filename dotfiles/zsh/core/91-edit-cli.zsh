e4282deffeed69a6d725429c50bef9afe5d2fb34
Daniel Duckworth <duckworthd@gmail.com>
2017-09-13 14:22:55 -0700

    zsh: edit command with vim in vim-style bindings

--- dotfiles/zsh/core/91-edit-cli.zsh
+++ dotfiles/zsh/core/91-edit-cli.zsh
@@ -1,4 +1,9 @@
-# Pressing C-x C-e allows user to edit command in the default editor.
 autoload -z edit-command-line
 zle -N edit-command-line
+
+# Press C-x C-e allows user to edit command in the default editor if using
+# emacs style key bindings.
 bindkey "^X^E" edit-command-line
+
+# Press Esc-v for the same with vim-style bindings.
+bindkey -M vicmd v edit-command-line
