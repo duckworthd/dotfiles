378c369198cd068ca47b47dd13dbdbb73091b466
Daniel Duckworth <duckworthd@gmail.com>
2018-01-23 11:11:11 -0800

    fish: swap escape/caps, append to /usr/local/google/home/duckworthd/bin /usr/local/google/home/duckworthd/anaconda2/bin /usr/local/google/home/duckworthd/bin /usr/lib/google-golang/bin /usr/local/buildtools/java/jdk/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -22,14 +22,21 @@
 # A-c      # find path, cd into it.
 # A-e      # open command in $EDITOR.
 
-# Setup $PATH
-set -gx PATH $HOME/bin $HOME/anaconda2/bin $PATH
+# Setup $PATH.
+#
+# Contents of $fish_user_paths are appended to $PATH.
+set extra_paths $HOME/bin $HOME/anaconda2/bin
+for extra_path in extra_paths
+  if test -e $extra_path
+    set --universal fish_user_paths $extra_path $fish_user_paths
+  end
+end
 
 # Use vim for command line editor.
 set EDITOR (which vim)" -u NONE"
 
 # Use vim-style command line editing.
-set -g fish_key_bindings fish_vi_key_bindings
+set --global fish_key_bindings fish_vi_key_bindings
 
 # Aliases
 alias rm="rm -i"
@@ -38,3 +45,8 @@ alias mv="mv -i"
 
 # No greeting message.
 set fish_greeting ""
+
+# Swap caps lock, escape.
+if type -q setxkbmap
+  setxkbmap -option caps:swapescape
+end
